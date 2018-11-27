d197a738bbbcac67e0949aeddc7a739768d3a6cc
adrien <adriendomecq@gmail.com>
2018-11-27 11:42:14 +0100

    lol

---
+++ rewrite.php
@@ -0,0 +1,16 @@
+<?php
+
+echo $_SERVER['REDIRECT_URL'];
+
+if(preg_match('#news-([0-9-]+)\.html#isU', $_SERVER['REDIRECT_URL'], $match)){
+	header("Status: 200 OK", false, 200);
+	echo 'lol';
+}
+else{
+	echo "rip";
+}
+//echo "lol";
+
+echo $_SERVER['REDIRECT_URL'];
+
+
--- view/header.php
+++ view/header.php
@@ -14,6 +14,7 @@
     </head>
 
     <?php if (session_status() == PHP_SESSION_NONE) session_start() ?>
+    <?php //var_dump($_SERVER); ?>
     
 
     <body>
