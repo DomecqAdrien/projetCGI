999982a7eb4bf8066a30163e0a5b9e479dc80125
Adrien <adrien7728@gmail.com>
2018-11-29 21:53:23 +0100

    a

---
+++ view/admin/update.php
@@ -0,0 +1,20 @@
+<?php $title = "Modifier un utilisateur" ?>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6">
+      <div class="card bg-default">
+        <h5 class="card-header">
+          Modifier l'utilisateur: 
+        </h5>
+        <div class="card-body">
+          <form method="POST">
+            <textarea name="commentaire" rows="2" class="form-control"></textarea>
+            <br>
+            <button type="submit" class="btn btn-outline-primary" value="Envoyer">Envoyer</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
