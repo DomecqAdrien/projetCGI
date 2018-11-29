f8a6cb19d0cb2bb7cecfd93bc1cdd160a94e510b
Julien Maillard <j.maillard77250@gmail.com>
2018-11-29 16:22:50 +0100

    o

--- Controller/ChatController.php
+++ Controller/ChatController.php
@@ -11,7 +11,8 @@ class ChatController extends Controller{
 
     function ajax(){
     	$this->loadModel('Chat');
-    	if (isset($_POST['id_to']) && isset($_POST['message'])){
+
+		if (isset($_POST['id_to']) && isset($_POST['message'])){
     		$chat = new Chat();
     		$chat->setId_user_to($_POST['id_to']);
     		$chat->setId_user_from($_SESSION['id']);
@@ -24,15 +25,10 @@ class ChatController extends Controller{
     		$data['msgs_user'] = Chat::getAllMessages($_SESSION['id'],$_POST['id_to']);
     		$this->set($data);
     	}
+
     	if (isset($_POST['id'])) {
     		$data['msgs_user'] = Chat::getAllMessages($_SESSION['id'],$_POST['id']);
     		$this->set($data);
 	    }
-    }
-    function test(){
-    	$this->loadModel('Chat');
-    	$msg_user1 = Chat::getAllMessages('5','3');
-    	debug($msgs_user1);
-    	echo $msgs_user1[0]->id;
     }
 }
--- components/js/chat.js
+++ components/js/chat.js
@@ -7,12 +7,34 @@ $('.chat_list').click(function(){
     type: 'post',
     data: { 'id': $('.id', this).attr('id')},
     success: function(response) {
-    	$('.msgs').html(response); }
+    	$('.msgs').html(response);
+    	$('.msg_history').scrollTop($('.msg_history')[0].scrollHeight)
+    	}
 	});
 });
 
-$('.write_msg').focus(function(){
-	$(document).keyup(function(e){
+$('.msg_send_btn').click(function(e){
+	e.preventDefault();
+	if ($('.write_msg').val() != ''){
+		$.ajax({
+			url: 'chat/ajax',
+			type: 'post',
+			data: { 
+				'id_to': $('.active_chat').find('.id').attr('id'),
+				'message': $('.write_msg').val()
+			},
+			success: function(response) {
+				$('.write_msg').val('');
+				$('.msgs').html(response);
+				$('.msg_history').scrollTop($('.msg_history')[0].scrollHeight)
+			}
+		});
+	}
+});
+
+$(document).keyup(function(e){
+	e.preventDefault(); 
+	if ($('.write_msg').val() != ''){
 		if (e.which == 13){
 			$.ajax({
 				url: 'chat/ajax',
@@ -21,11 +43,14 @@ $('.write_msg').focus(function(){
 					'id_to': $('.active_chat').find('.id').attr('id'),
 					'message': $('.write_msg').val()
 				},
-			success: function(response) {
-				$('.msgs').html(response);}
+				success: function(response) {
+					$('.write_msg').val('');
+					$('.msgs').html(response);
+					$('.msg_history').scrollTop($('.msg_history')[0].scrollHeight)
+				}
 			});
 		}
-	});
+	}
 });
 
 window.setInterval(function(){
